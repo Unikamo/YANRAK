760c82fbc28f60119180231fa6c3044cf623b6f6
Unikamo <foxtrot.4874@gmail.com>
2024-10-12 10:58:20 +0200

    Updated boot.s with comments
    Added comments to boot.s so we (I) understand the code better

--- boot.s
+++ boot.s
@@ -1,5 +1,8 @@
 ; boot is loaded at 0x7c00
 ; must fit in 512 bytes sector
+; [somethings] is a (dereferenced) pointer* to something
+
+
 
 bits 16
 org 0x7c00 ; move to 0x7c00
@@ -9,17 +12,18 @@ mov si, 0
 printchar:
 	mov ah, 0x0e ; print a character
 	mov al, [hello + si] ; prints "Hello, World!" + si register
-	int 0x10 ; Video Services interrupt
-	add si, 1
+	int 0x10 ; Video Services interrupt (BIOS Interrupt)
+	add si, 1 ; Prints the next letter
 	cmp byte [hello + si], 0 ; check if the memory contains 0
 	jne printchar ; if not, print the next char
 
-jmp $
+jmp $ ; $ = current memory adress
 
 hello:
-	db "Hello, World!", 0
-
-times 510 - ($ - $$) db 0 ; fill 510 minus start of the line minus start of the program of zeroes
+	db "Hello, World!", 0 ; 0 = null termination
+; $$ means "start of current section"
+times 510-($-$$) db 0 ; fill 510 minus start of the line minus start of the program of zeroes
+			; db means "define byte", it adds bytes
 dw 0xAA55 ; every x86 boot sector must end with it (takes 2 bytes)
 
 
